fc1a9d48cd03f72db150a5a1494ece1eb294844b
bharathkumar.kandaswamy1@t-mobile.com <bharathkumar.kandaswamy1@t-mobile.com>
2017-09-13 15:40:42 -0700

    Working App - get Method Works(Before Mongo Integration)

--- src/main/java/my/abs2/propro/example/demo/PasteItApplication.java
+++ src/main/java/my/abs2/propro/example/demo/PasteItApplication.java
@@ -1,8 +1,15 @@
 package my.abs2.propro.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import java.util.concurrent.TimeUnit;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.http.HttpStatus;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -18,4 +25,13 @@ public class PasteItApplication {
 			System.out.println(beanName);
 		}
 	}
+	
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+	    TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+	    factory.setPort(9876);
+	    factory.setSessionTimeout(10, TimeUnit.MINUTES);
+	    factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
+	    return factory;
+	} 
 }
