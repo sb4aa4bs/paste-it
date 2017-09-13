bf87bf668ad80c5030c8db4546b1a121fb2bfdb7
bharathkumar.kandaswamy1@t-mobile.com <bharathkumar.kandaswamy1@t-mobile.com>
2017-09-13 12:02:08 -0700

    Initial

---
+++ src/main/java/my/abs2/propro/example/demo/PasteItApplication.java
@@ -0,0 +1,12 @@
+package my.abs2.propro.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteItApplication.class, args);
+	}
+}
