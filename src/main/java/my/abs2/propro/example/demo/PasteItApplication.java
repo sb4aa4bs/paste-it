6ce622c9c532589144293c559a51c2dd301dffb3
bharathkumar.kandaswamy1@t-mobile.com <bharathkumar.kandaswamy1@t-mobile.com>
2017-09-13 14:14:21 -0700

    Initial

--- src/main/java/my/abs2/propro/example/demo/PasteItApplication.java
+++ src/main/java/my/abs2/propro/example/demo/PasteItApplication.java
@@ -2,11 +2,20 @@ package my.abs2.propro.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class PasteItApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PasteItApplication.class, args);
+		// SpringApplication.run(PasteItApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(PasteItApplication.class, args);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		System.out.println("**************************************");
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 }
